dd4b772b346a7368dde17239df9bb76a0d9ea30c
Kirk Bushell <torm3nt@gmail.com>
2015-10-23 15:49:00 +0100

    Pulled error handling from flarum/core
    Added first test for exception handler
    
    Added status code check for response bag
    
    Added status code check for response bag
    
    Tests for response without debug mode
    
    Added test for debug mode

---
+++ src/Exception/Handler/FallbackExceptionHandler.php
@@ -0,0 +1,62 @@
+<?php
+namespace Tobscure\JsonApi\Exception\Handler;
+
+use Exception;
+use Flarum\Core;
+
+class FallbackExceptionHandler implements ExceptionHandler
+{
+    /**
+     * @var bool
+     */
+    private $debug;
+
+    /**
+     * @param bool $debug
+     */
+    public function __construct($debug)
+    {
+        $this->debug = $debug;
+    }
+    /**
+     * If the exception handler is able to format a response for the provided exception,
+     * then the implementation should return true.
+     *
+     * @param Exception $e
+     * @return boolean
+     */
+    public function manages(Exception $e)
+    {
+        return true;
+    }
+
+    /**
+     * Handle the provided exception.
+     *
+     * @param Exception $e
+     * @return mixed
+     */
+    public function handle(Exception $e)
+    {
+        $status = 500;
+        $error = $this->constructError($e, $status);
+
+        return new ResponseBag($status, [$error]);
+    }
+
+    /**
+     * @param Exception $e
+     * @param $status
+     * @return array
+     */
+    private function constructError(Exception $e, $status)
+    {
+        $error = ['code' => $status, 'title' => 'Internal server error'];
+
+        if ($this->debug) {
+            $error['detail'] = (string) $e;
+        }
+
+        return $error;
+    }
+}
---
+++ tests/Exception/Handler/FallbackExceptionHandlerTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Tobscure\Tests\Exception\Handler;
+
+use Exception;
+use Tobscure\JsonApi\Exception\Handler\FallbackExceptionHandler;
+use Tobscure\JsonApi\Exception\Handler\ResponseBag;
+
+class FallbackExceptionHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandlerCanManageExceptions()
+    {
+        $handler = new FallbackExceptionHandler(false);
+
+        $this->assertTrue($handler->manages(new Exception));
+    }
+
+    public function testErrorHandlingWithoutDebugMode()
+    {
+        $handler = new FallbackExceptionHandler(false);
+        $response = $handler->handle(new Exception);
+
+        $this->assertInstanceOf(ResponseBag::class, $response);
+        $this->assertEquals(500, $response->getStatus());
+        $this->assertEquals([['code' => 500, 'title' => 'Internal server error']], $response->getErrors());
+    }
+
+    public function testErrorHandlingWithDebugMode()
+    {
+        $handler = new FallbackExceptionHandler(true);
+        $response = $handler->handle(new Exception);
+        
+        $this->assertInstanceOf(ResponseBag::class, $response);
+        $this->assertEquals(500, $response->getStatus());
+        $this->assertArrayHasKey('detail', $response->getErrors()[0]);
+    }
+}
